ab249deb948859a6b120182f4690b9303924e633
renanNun <renannunes400@gmail.com>
2020-08-02 22:30:39 -0300

    Upload basic files

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express"); //Importando o Express
+const app = express();
+const bodyParser = require("body-parser"); //Importando o Body-Parser
+
+
+//Informando o express para usar o EJS como engine na criação de HTML
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+//Body-Parser
+app.use(bodyParser.urlencoded ({ extended: false }));
+app.use(bodyParser.json());
+
+//Rotas
+app.get("/", (req,res) => {
+    res.render("index.ejs");
+});
